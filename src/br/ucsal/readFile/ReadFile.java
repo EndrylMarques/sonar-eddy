7b5a76f62d18a0ea68547f68c7841fafe0660b27
EndrylMarques <endryl.marques@gmail.com>
2019-04-01 10:03:37 -0300

    Add file reader class

--- src/br/ucsal/readFile/ReadFile.java
+++ src/br/ucsal/readFile/ReadFile.java
@@ -1,8 +1,29 @@
 package br.ucsal.readFile;
 
+import java.io.File;
+
 public class ReadFile {
 
     public static void main(String[] args) {
+        getFile("C:\\leitor");
+    }
 
+
+
+    private static void getFile(String dirPath) {
+        int count = 0;
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+        if (files != null)
+            for (int i = 0; i < files.length; i++) {
+                count++;
+                File file = files[i];
+
+                if (file.isDirectory()) {
+                    getFile(file.getAbsolutePath());
+                }
+                System.out.println(file.getName());
+            }
     }
 }
